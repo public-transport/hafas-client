5b0a4034cfa9f4edcb390c9bead2a3c17c8da177
Jannis R <mail@jannisr.de>
2016-06-25 20:54:33 +0200

    parse date & time :watch:

--- parse.js
+++ parse.js
@@ -1,5 +1,20 @@
 'use strict'
 
+const moment = require('moment-timezone')
+
+
+
+const dateTime = (timezone) => (date, time) => {
+	let offset = 0 // in days
+	if (time.length > 6) {
+		offset = +time.slice(0, -6)
+		time = time.slice(-6)
+	}
+	return moment.tz(date + 'T' + time, timezone)
+	.add(offset, 'days')
+	.valueOf()
+}
+
 
 
 const types = {P: 'poi', S: 'station', A: 'address'}
