f86f908dcc2972c46b3021b4a98703c73e3f0ed7
Jannis R <mail@jannisr.de>
2017-11-12 18:06:16 +0100

    query departures

--- index.js
+++ index.js
@@ -9,8 +9,40 @@ const createClient = (profile) => {
 		throw new Error('profile.timezone must be a string.')
 	}
 
-	const client = data => request(profile, data)
-	return client
+	const departures = (station, opt = {}) => {
+		if ('string' !== typeof station) throw new Error('station must be a string.')
+
+		opt = Object.assign({
+			direction: null, // only show departures heading to this station
+			duration:  10 // show departures for the next n minutes
+		}, opt)
+		opt.when = opt.when || new Date()
+		const products = profile.formatProducts(opt.products || {})
+
+		const dir = opt.direction ? profile.formatStation(opt.direction) : null
+		return request(profile, {
+			meth: 'StationBoard',
+			req: {
+	        	type: 'DEP',
+				date: profile.formatDate(profile, opt.when),
+				time: profile.formatTime(profile, opt.when),
+				stbLoc: profile.formatStation(station),
+				dirLoc: dir,
+				jnyFltrL: [
+					profile.formatProducts(opt.products) // todo
+				],
+				dur: opt.duration,
+		        getPasslist: false
+			}
+		})
+		.then((d) => {
+			if (!Array.isArray(d.jnyL)) return [] // todo: throw err?
+			const parse = profile.parseDeparture(profile, d.locations, d.lines, d.remarks)
+			return d.jnyL.map(parse)
+		})
+	}
+
+	return {departures}
 }
 
 module.exports = createClient
