b0eb6d6474fab589007fbe05793913eecc408750
Julius Tens <mail@juliustens.eu>
2017-12-28 16:50:30 +0100

    fix requests with no filters

--- format/products-bitmask.js
+++ format/products-bitmask.js
@@ -2,6 +2,7 @@
 
 const createFormatBitmask = (modes) => {
 	const formatBitmask = (products) => {
+		if(Object.keys(products).length === 0) return null
 		let bitmask = 0
 		for (let product in products) {
 			if (products[product] === true) bitmask += modes[product].bitmask
