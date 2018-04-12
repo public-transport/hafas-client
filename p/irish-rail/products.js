8ed218f4d62a0c220d453b1b1ffa7ce232f1bb83
Jannis R <mail@jannisr.de>
2018-04-12 21:49:55 +0200

    add Irish Rail profile ✨

---
+++ p/irish-rail/products.js
@@ -0,0 +1,38 @@
+'use strict'
+
+module.exports = [
+	{
+		id: 'national-train',
+		mode: 'train',
+		bitmasks: [2],
+		name: 'InterCity',
+		short: 'IC',
+		default: true
+	},
+	// todo: 4
+	{
+		id: 'local-train',
+		mode: 'train',
+		bitmasks: [8],
+		name: 'Commuter',
+		short: 'Commuter',
+		default: true
+	},
+	{
+		id: 'suburban',
+		mode: 'train',
+		bitmasks: [16],
+		name: 'Dublin Area Rapid Transit',
+		short: 'DART',
+		default: true
+	},
+	// todo: 32
+	{
+		id: 'luas',
+		mode: 'train',
+		bitmasks: [64],
+		name: 'LUAS Tram',
+		short: 'LUAS',
+		default: true
+	}
+]
