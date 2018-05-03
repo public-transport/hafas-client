47aea604df3d2949840b7f8fcb7750dd2bfb20a4
Jannis R <mail@jannisr.de>
2018-05-03 10:29:43 +0200

    fix error message :bug:

--- format/location.js
+++ format/location.js
@@ -7,7 +7,7 @@ const formatLocation = (profile, l, name = 'location') => {
 		if ('string' === typeof l.id) return profile.formatPoi(l)
 		if ('string' === typeof l.address) return profile.formatAddress(l)
 		if (!l.type) throw new Error(`missing ${name}.type`)
-		throw new Error(`invalid ${name}type: ${l.type}`)
+		throw new Error(`invalid ${name}.type: ${l.type}`)
 	}
 	throw new Error(name + ': valid station, address or poi required.')
 }
