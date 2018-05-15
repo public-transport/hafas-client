9a0bfc39a45fa69c01dfa0365e34c746c5e43309
Jannis R <mail@jannisr.de>
2018-05-15 19:39:28 +0200

    radar: polylines option, return polylines

--- parse/movement.js
+++ parse/movement.js
@@ -1,13 +1,12 @@
 'use strict'
 
-const createParseMovement = (profile, locations, lines, remarks) => {
+const createParseMovement = (profile, locations, lines, remarks, polylines) => {
 	// todo: what is m.dirGeo? maybe the speed?
 	// todo: what is m.stopL?
 	// todo: what is m.proc? wut?
 	// todo: what is m.pos?
 	// todo: what is m.ani.dirGeo[n]? maybe the speed?
 	// todo: what is m.ani.proc[n]? wut?
-	// todo: how does m.ani.poly work?
 	const parseMovement = (m) => {
 		const pStopover = profile.parseStopover(profile, locations, lines, remarks, m.date)
 
@@ -24,13 +23,23 @@ const createParseMovement = (profile, locations, lines, remarks) => {
 			frames: []
 		}
 
-		if (m.ani && Array.isArray(m.ani.mSec)) {
-			for (let i = 0; i < m.ani.mSec.length; i++) {
-				res.frames.push({
-					origin: locations[m.ani.fLocX[i]] || null,
-					destination: locations[m.ani.tLocX[i]] || null,
-					t: m.ani.mSec[i]
-				})
+		if (m.ani) {
+			if (Array.isArray(m.ani.mSec)) {
+				for (let i = 0; i < m.ani.mSec.length; i++) {
+					res.frames.push({
+						origin: locations[m.ani.fLocX[i]] || null,
+						destination: locations[m.ani.tLocX[i]] || null,
+						t: m.ani.mSec[i]
+					})
+				}
+			}
+
+			if (m.ani.poly && m.ani.poly.crdEncYX) {
+				res.polyline = m.ani.poly.crdEncYX
+			} else if (m.ani.polyG && Array.isArray(m.ani.polyG.polyXL)) {
+				let p = m.ani.polyG.polyXL[0]
+				// todo: there can be >1 polyline
+				res.polyline = polylines[p] && polylines[p].crdEncXY || null
 			}
 		}
 
