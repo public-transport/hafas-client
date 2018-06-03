440235031b0bbefc98d82d8b5d385abe470c6d05
Nick Turskyi <nick@nsfw.ninja>
2018-06-03 17:30:14 -0500

    CMTA: correct examples, enabled radar

--- p/cmta/example.js
+++ p/cmta/example.js
@@ -6,13 +6,12 @@ const cmtaProfile = require('.')
 const client = createClient(cmtaProfile)
 
 // Broadie Oaks to Domain
-client.journeys('000002370', '900000024101', {results: 1, polylines: true})
+client.journeys('000002370', '000005919', {results: 1, polylines: true})
 // client.departures('000002370', {duration: 1})
 // client.locations('Westgate', {results: 2})
-// client.location('900000042101') // Spichernstr TODO
-// client.nearby(52.5137344, 13.4744798, {distance: 60}) TODO
-// client.radar(52.52411, 13.41002, 52.51942, 13.41709, {results: 10}) TODO
-
+// client.location('000005534') // Downtown light rail station
+// client.nearby(30.266222, -97.746058, {distance: 60})
+// client.radar(30.240877, -97.804588, 30.225378, -97.786692, {results: 10})
 // .then(([journey]) => {
 // 	const leg = journey.legs[0]
 // 	return client.journeyLeg(leg.id, leg.line.name, {polyline: true})
