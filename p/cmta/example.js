773d6d77d782d6ab090765f2f7529e458023ae69
Nick Turskyi <nick@nsfw.ninja>
2018-06-04 08:30:24 -0500

    CMTA: Corrected "nearby" example, enabled journeyLeg

--- p/cmta/example.js
+++ p/cmta/example.js
@@ -10,7 +10,11 @@ client.journeys('000002370', '000005919', {results: 1, polylines: true})
 // client.departures('000002370', {duration: 1})
 // client.locations('Westgate', {results: 2})
 // client.location('000005534') // Downtown light rail station
-// client.nearby(30.266222, -97.746058, {distance: 60})
+// client.nearby({
+//    type: 'location',
+//    latitude: 30.266222,
+//    longtitude: -97.746058
+// }, {distance: 60})
 // client.radar(30.240877, -97.804588, 30.225378, -97.786692, {results: 10})
 // .then(([journey]) => {
 // 	const leg = journey.legs[0]
