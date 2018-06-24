02cdb1c497f2748719859f891a5040d2c5a15a20
guitar9 <t.kwant1@gmail.com>
2018-06-24 23:47:49 +0200

    Update date.js

--- format/date.js
+++ format/date.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const formatDate = (profile, when) => {
-	let date = new Date(when)
-	var month = String(date.getUTCMonth() + 1); //months from 1-12
-	var day = String(date.getUTCDate());
-	var year = String(date.getUTCFullYear());
+	var date = new Date(when)
+	var month = String(date.getMonth() + 1); //months from 1-12
+	var day = String(date.getDate()));
+	var year = String(date.getFullYear());
 	if(day <10){
 		day = "0"+ day
 	}
