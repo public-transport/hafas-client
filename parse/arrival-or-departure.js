83ede3e49a73dcb5e7188723b46bc6dc951dc67d
Jannis R <mail@jannisr.de>
2018-06-26 15:49:50 +0200

    parse arrivals

---
+++ parse/arrival-or-departure.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const findRemark = require('./find-remark')
+
+// todo: what is d.jny.dirFlg?
+// todo: d.stbStop.dProgType/d.stbStop.aProgType
+
+const createParseArrOrDep = (profile, opt, data, prefix) => {
+	const {locations, lines, hints, warnings} = data
+	if (prefix !== 'a' && prefix !== 'd') throw new Error('invalid prefix')
+
+	const parseArrOrDep = (d) => {
+		const t = d.stbStop[prefix + 'TimeR'] || d.stbStop[prefix + 'TimeS']
+		const when = profile.parseDateTime(profile, d.date, t)
+
+		const res = {
+			tripId: d.jid,
+			station: locations[parseInt(d.stbStop.locX)] || null,
+			when: when.toISO(),
+			direction: profile.parseStationName(d.dirTxt),
+			line: lines[parseInt(d.prodX)] || null,
+			remarks: (d.remL
+				? d.remL.map(ref => findRemark(hints, warnings, ref))
+				: []
+			),
+			// todo: res.trip from rawLine.prodCtx.num?
+			trip: +d.jid.split('|')[1] // todo: this seems brittle
+		}
+
+		// todo: DRY with parseStopover
+		// todo: DRY with parseJourneyLeg
+		const tR = d.stbStop[prefix + 'TimeR']
+		const tP = d.stbStop[prefix + 'TimeS']
+		if (tR && tP) {
+			const realtime = profile.parseDateTime(profile, d.date, tR)
+			const planned = profile.parseDateTime(profile, d.date, tP)
+			res.delay = Math.round((realtime - planned) / 1000)
+		} else res.delay = null
+
+		// todo: DRY with parseStopover
+		// todo: DRY with parseJourneyLeg
+		if (d.stbStop[prefix + 'Cncl']) {
+			res.cancelled = true
+			Object.defineProperty(res, 'canceled', {value: true})
+			res.when = res.delay = null
+
+			const when = profile.parseDateTime(profile, d.date, tP)
+			res.formerScheduledWhen = when.toISO()
+		}
+
+		return res
+	}
+
+	return parseArrOrDep
+}
+
+module.exports = createParseArrOrDep
--- parse/departure.js
+++ parse/departure.js
@@ -1,55 +1,10 @@
 'use strict'
 
-const findRemark = require('./find-remark')
+const createParseArrOrDep = require('./arrival-or-departure')
 
-// todos from public-transport/hafas-client#2
-// - stdStop.dPlatfS, stdStop.dPlatfR
-// todo: what is d.jny.dirFlg?
-// todo: d.stbStop.dProgType
-// todo: d.freq, d.freq.jnyL, see https://github.com/public-transport/hafas-client/blob/9203ed1481f08baacca41ac5e3c19bf022f01b0b/parse.js#L115
-
+const DEPARTURE = 'd'
 const createParseDeparture = (profile, opt, data) => {
-	const {locations, lines, hints, warnings} = data
-
-	const parseDeparture = (d) => {
-		const when = profile.parseDateTime(profile, d.date, d.stbStop.dTimeR || d.stbStop.dTimeS)
-		const res = {
-			tripId: d.jid,
-			station: locations[parseInt(d.stbStop.locX)] || null,
-			when: when.toISO(),
-			direction: profile.parseStationName(d.dirTxt),
-			line: lines[parseInt(d.prodX)] || null,
-			remarks: (d.remL
-				? d.remL.map(ref => findRemark(hints, warnings, ref))
-				: []
-			),
-			trip: +d.jid.split('|')[1] // todo: this seems brittle
-		}
-		// todo: res.trip from rawLine.prodCtx.num?
-
-		// todo: DRY with parseStopover
-		// todo: DRY with parseJourneyLeg
-		if (d.stbStop.dTimeR && d.stbStop.dTimeS) {
-			const realtime = profile.parseDateTime(profile, d.date, d.stbStop.dTimeR)
-			const planned = profile.parseDateTime(profile, d.date, d.stbStop.dTimeS)
-			res.delay = Math.round((realtime - planned) / 1000)
-		} else res.delay = null
-
-		// todo: DRY with parseStopover
-		// todo: DRY with parseJourneyLeg
-		if (d.stbStop.aCncl || d.stbStop.dCncl) {
-			res.cancelled = true
-			Object.defineProperty(res, 'canceled', {value: true})
-			res.when = res.delay = null
-
-			const when = profile.parseDateTime(profile, d.date, d.stbStop.dTimeS)
-			res.formerScheduledWhen = when.toISO()
-		}
-
-		return res
-	}
-
-	return parseDeparture
+	return createParseArrOrDep(profile, opt, data, DEPARTURE)
 }
 
 module.exports = createParseDeparture
