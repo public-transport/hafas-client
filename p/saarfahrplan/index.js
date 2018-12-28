cb599748fbabab7d6f662a318e737bd69d87c357
Jannis R <mail@jannisr.de>
2018-12-28 15:52:29 +0100

    Saarfahrplan profile

---
+++ p/saarfahrplan/index.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const products = require('./products')
+
+const transformReqBody = (body) => {
+	body.client = {
+		type: 'AND',
+		id: 'ZPS-SAAR',
+		v: 1000070,
+		name: 'Saarfahrplan',
+		os: 'Android 9'
+	}
+	body.ver = '1.21'
+	body.auth = {type: 'AID', aid: '51XfsVqgbdA6oXzHrx75jhlocRg6Xe'}
+	body.lang = 'de'
+
+	return body
+}
+
+const saarfahrplanProfile = {
+	locale: 'de-DE',
+	timezone: 'Europe/Berlin',
+	endpoint: 'https://saarfahrplan.de/bin/mgate.exe',
+	transformReqBody,
+
+	// decrypted form of https://gist.github.com/derhuerst/f5a8c07f9b3226ecad6c8f64d83df6a2#file-haf_config_base-properties-L70
+	salt: Buffer.from('HJtlubisvxiJxss', 'utf8'),
+	addMicMac: true,
+
+	products: products,
+
+	trip: true,
+	radar: true,
+	reachableFrom: true
+}
+
+module.exports = saarfahrplanProfile
