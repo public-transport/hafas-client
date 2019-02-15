dafc96ad113048dd8bc5fe906707fec7e6432f1e
Jannis R <mail@jannisr.de>
2019-02-15 14:57:05 +0100

    update CMTA credentials

--- p/cmta/index.js
+++ p/cmta/index.js
@@ -4,15 +4,13 @@ const products = require('./products')
 
 const transformReqBody = (body) => {
 	body.client = {
-		type: 'WEB',
+		type: 'IPH',
 		id: 'CMTA',
-		name: 'webapp',
-		l: '' // todo: what is this?
+		v: '2',
+		name: 'CapMetro'
 	}
-	body.ext = 'SBB.TZT.1'
 	body.ver = '1.13'
-	body.auth = {type: 'AID', aid: 'weblwemrcrlwemlcri'}
-	// todo: `body.id = 'ztgcgxywk88bgm88'`
+	body.auth = {type: 'AID', aid: 'ioslaskdcndrjcmlsd'}
 
 	return body
 }
