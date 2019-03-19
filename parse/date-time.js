5d49fd0a200c43675975dab30bf4948e82dceb94
Jannis R <mail@jannisr.de>
2019-03-19 11:51:35 +0100

    parseDateTime: fix tzOffset + daysOffset :bug:
    closes #106

--- parse/date-time.js
+++ parse/date-time.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const {DateTime, IANAZone} = require('luxon')
+const {DateTime, FixedOffsetZone, IANAZone} = require('luxon')
 
 const timezones = new WeakMap()
 
@@ -19,26 +19,21 @@ const parseDateTime = (profile, date, time, tzOffset = null, timestamp = false) 
 	const daysOffset = time.length > 6 ? parseInt(time.slice(0, -6)) : 0
 
 	let timezone
-	if (timezones.has(profile)) timezone = timezones.get(profile)
-	else {
+	if (tzOffset !== null) {
+		timezone = FixedOffsetZone.instance(tzOffset)
+	} else if (timezones.has(profile)) {
+		timezone = timezones.get(profile)
+	} else {
 		timezone = new IANAZone(profile.timezone)
 		timezones.set(profile, timezone)
 	}
 
-	if (tzOffset !== null) {
-		// We don't know the timezone, but only the *timezone offset*, which is why we
-		// can't use Luxon to process the offset.
-		const isoOffset = ('0' + (tzOffset / 60 | 0)).slice(-2) + ('0' + (tzOffset % 60)).slice(-2)
-		const isoStr = pDate.join('-') + 'T' + pTime.join(':') + '+' + isoOffset
-		return timestamp ? +new Date(isoStr) : isoStr
-	} else {
-		let dt = DateTime.fromISO(pDate.join('-') + 'T' + pTime.join(':'), {
-			locale: profile.locale,
-			zone: timezone
-		})
-		if (daysOffset > 0) dt = dt.plus({days: daysOffset})
-		return timestamp ? dt.toMillis() : dt.toISO()
-	}
+	let dt = DateTime.fromISO(pDate.join('-') + 'T' + pTime.join(':'), {
+		locale: profile.locale,
+		zone: timezone
+	})
+	if (daysOffset > 0) dt = dt.plus({days: daysOffset})
+	return timestamp ? dt.toMillis() : dt.toISO()
 }
 
 module.exports = parseDateTime
