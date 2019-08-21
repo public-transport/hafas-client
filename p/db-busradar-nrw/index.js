b7a6dbc504d604df6dff80444772d259c5c1e889
Kevin Arutyunyan <d3d9@riseup.net>
2019-08-21 15:04:47 +0200

    Use HTTPS for DB Busradar NRW endpoint

--- p/db-busradar-nrw/index.js
+++ p/db-busradar-nrw/index.js
@@ -105,7 +105,7 @@ const transformReqBody = (body) => {
 const dbBusradarNrwProfile = {
 	locale: 'de-DE',
 	timezone: 'Europe/Berlin',
-	endpoint: 'http://db-regio.hafas.de/bin/hci/mgate.exe',
+	endpoint: 'https://db-regio.hafas.de/bin/hci/mgate.exe',
 	transformReqBody,
 
 	products: products,
