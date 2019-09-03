521a2faa70258bf7e43ceb198d5c853eac08a7b1
Jannis R <mail@jannisr.de>
2019-09-03 15:33:30 +0200

    parseHint: parse code & text properly :bug:

--- parse/hint.js
+++ parse/hint.js
@@ -12,6 +12,7 @@ const codesByIcon = Object.assign(Object.create(null), {
 // "tagL": [
 // 	"RES_JNY_DTL" // only shown in journey detail
 // ]
+// todo: https://github.com/public-transport/hafas-client/issues/5
 const parseHint = (profile, h, _) => {
 	// todo: C
 	// todo:
@@ -46,8 +47,8 @@ const parseHint = (profile, h, _) => {
 	if (h.type === 'A' || h.type === 'I') {
 		return {
 			type: 'hint',
-			code: h.code || null,
-			text: h.txtN || null
+			code,
+			text
 		}
 	}
 
