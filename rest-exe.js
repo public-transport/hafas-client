236418d899888c9d617dcb92ca923eb5a570c8e1
Jannis R <mail@jannisr.de>
2019-11-06 13:31:29 +0100

    rest.exe: add request()

--- rest-exe.js
+++ rest-exe.js
@@ -2,6 +2,13 @@
 
 const DEBUG = process.env.DEBUG === 'hafas-client'
 
+const Promise = require('pinkie-promise')
+const {fetch} = require('fetch-ponyfill')({Promise})
+const {stringify} = require('qs')
+const {parse: parseContentType} = require('content-type')
+const randomizeUserAgent = require('./lib/randomize-user-agent')
+const {byErrorCode} = require('./lib/rest-exe-errors')
+
 const isNonEmptyString = str => 'string' === typeof str && str.length > 0
 
 const createRestClient = (profile, token, userAgent) => {
@@ -10,7 +17,54 @@ const createRestClient = (profile, token, userAgent) => {
 	if (!isNonEmptyString(userAgent)) throw new Error('missing userAgent')
 
 	const request = async (method, opt, query = {}) => {
-		// todo
+		query = {
+			lang: opt.language || 'en',
+			...query,
+			format: 'json'
+		}
+		if (DEBUG) console.error(JSON.stringify(query))
+
+		const url = profile.endpoint + method + '?' + stringify({...query, accessId: token})
+		const fetchCfg = {
+			headers: {
+				'accept-encoding': 'gzip, br, deflate',
+				'accept': 'application/json',
+				'user-agent': randomizeUserAgent(userAgent)
+			},
+			redirect: 'follow'
+		}
+		const res = await fetch(url, fetchCfg)
+
+		const cTypeHeader = res.headers.get('content-type')
+		const {type: cType} = cTypeHeader ? parseContentType(cTypeHeader) : {}
+		const asJSON = cType === 'application/json'
+		const body = asJSON ? await res.json() : await res.text()
+		if (DEBUG) console.error(asJSON ? JSON.stringify(body) : body)
+
+		if (!res.ok) {
+			// todo: parse HTML error messages
+			let err = new Error(res.statusText)
+			if (asJSON) {
+				const {errorCode, errorText} = body
+				if (errorCode && byErrorCode[errorCode]) {
+					Object.assign(err, byErrorCode[errorCode])
+					err.hafasErrorCode = errorCode
+					if (errorText) err.hafasErrorMessage = errorText
+				} else {
+					err = new Error(errorText)
+					err.code = errorCode
+				}
+			} else if (body) err = new Error(body)
+
+			err.statusCode = res.status
+			err.endpoint = profile.endpoint
+			err.url = url
+			err.query = query
+			err.fetchCfg = fetchCfg
+			throw err
+		}
+
+		return body
 	}
 
 	return {}
