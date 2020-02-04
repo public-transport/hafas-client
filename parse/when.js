Bahn-X/hafas-rest-exe-client

parseWhen: make sure we have a date :bug:

parse/when.js
@@ -3,8 +3,8 @@
 const parseWhen = (profile, date, timeS, timeR, tzOffset, cncl = false) => {
 	const parse = profile.parseDateTime
 
-	let planned = timeS ? parse(profile, date, timeS, tzOffset, false) : null
-	let prognosed = timeR ? parse(profile, date, timeR, tzOffset, false) : null
+	let planned = date && timeS ? parse(profile, date, timeS, tzOffset, false) : null
+	let prognosed = date && timeR ? parse(profile, date, timeR, tzOffset, false) : null
 	let delay = null
 
 	if (planned && prognosed) {

public-transport/hafas-client

parse/when.js
@@ -3,8 +3,8 @@
 const parseWhen = (ctx, date, timeS, timeR, tzOffset, cncl = false) => {
 	const parse = ctx.profile.parseDateTime
 
-	let planned = timeS ? parse(ctx, date, timeS, tzOffset, false) : null
-	let prognosed = timeR ? parse(ctx, date, timeR, tzOffset, false) : null
+	let planned = date && timeS ? parse(ctx, date, timeS, tzOffset, false) : null
+	let prognosed = date && timeR ? parse(ctx, date, timeR, tzOffset, false) : null
 	let delay = null
 
 	if (planned && prognosed) {