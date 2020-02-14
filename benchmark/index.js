812bbb4dd2d55ad0cdd4cdf8b761c11a5dea6454
Jannis R <mail@jannisr.de>
2020-02-14 15:13:51 +0000

    basic parsing benchmark :zap:

---
+++ benchmark/index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const {Suite} = require('benchmark')
+
+const createClient = require('..')
+const dbProfile = require('../p/db')
+
+const client = createClient(dbProfile, 'public-transport/hafas-client:benchmark')
+const {profile} = client
+const manyDepartures = require('./many-departures.json')
+const mediumDepartures = require('./medium-departures.json')
+
+const suite = new Suite()
+suite
+
+.add('parse 1200 departures', () => {
+	const opt = {remarks: true}
+	const res = manyDepartures
+
+	const common = profile.parseCommon({profile, opt, res})
+	const ctx = {profile, opt, common, res}
+	res.jnyL.forEach(dep => profile.parseDeparture(ctx, dep))
+})
+
+.add('parse 45 departures', () => {
+	const opt = {remarks: true}
+	const res = mediumDepartures
+
+	const common = profile.parseCommon({profile, opt, res})
+	const ctx = {profile, opt, common, res}
+	res.jnyL.forEach(dep => profile.parseDeparture(ctx, dep))
+})
+
+.on('cycle', ({target}) => {
+	const perRun = Math.round(target.times.period * 100) / 100
+	console.log(`${target} (${perRun}s/run)`)
+})
+.run()
