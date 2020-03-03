acb33946de811fe02f92b4a5b5033df89e260fb5
Jannis R <mail@jannisr.de>
2020-03-03 02:36:26 +0100

    add VBN profile

---
+++ p/vbn/example.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const createClient = require('../..')
+const vbnProfile = require('.')
+
+const client = createClient(vbnProfile, 'hafas-client-example')
+
+const bremerhavenHbf = '9014418'
+const verden = '9093627'
+const bremenRutenstr = {
+	type: 'location',
+	id: '990025693',
+	address: 'Bremen Rutenstraße 1',
+	latitude: 53.074165, longitude: 8.8184
+}
+
+client.journeys(bremerhavenHbf, verden, {results: 1})
+// client.departures(bremerhavenHbf, {duration: 1})
+// client.arrivals(bremerhavenHbf, {duration: 10, linesOfStops: true})
+// client.locations('oldenburg', {results: 2})
+// client.stop(bremerhavenHbf, {linesOfStops: true})
+// client.nearby(bremenRutenstr)
+// client.radar({
+// 	north: 53.087,
+// 	west: 8.777,
+// 	south: 53.072,
+// 	east: 8.835
+// }, {results: 10})
+// client.reachableFrom(bremenRutenstr, {
+// 	when: new Date('2020-03-03T10:00:00+01:00'),
+// 	maxDuration: 10
+// })
+
+// .then(({journeys}) => {
+// 	const [journey] = journeys
+// 	const leg = journey.legs[0]
+// 	return client.trip(leg.tripId, leg.line.name, {polyline: true})
+// })
+
+// .then(({journeys}) => {
+// 	const [journey] = journeys
+// 	return client.refreshJourney(journey.refreshToken, {stopovers: true, remarks: true})
+// })
+
+.then((data) => {
+	console.log(require('util').inspect(data, {depth: null, colors: true}))
+})
+.catch(console.error)
--- readme.md
+++ readme.md
@@ -221,6 +221,7 @@ HAFAS endpoint | wrapper library | docs | example code | source code
 *DB Busradar NRW* ([DB Regio Bus](https://en.wikipedia.org/wiki/DB_Regio#Bus_division_(DB_Regio_Bus))) | - | [docs](p/db-busradar-nrw/readme.md) | [example code](p/db-busradar-nrw/example.js) | [src](p/db-busradar-nrw/index.js)
 [Verkehrsverbund Süd-Niedersachsen (VSN)](https://de.wikipedia.org/wiki/Verkehrsverbund_S%C3%BCd-Niedersachsen) | - | [docs](p/vsn/readme.md) | [example code](p/vsn/example.js) | [src](p/vsn/index.js)
 [Ingolstädter Verkehrsgesellschaft (INVG)](https://de.wikipedia.org/wiki/Ingolstädter_Verkehrsgesellschaft) | - | [docs](p/invg/readme.md) | [example code](p/invg/example.js) | [src](p/invg/index.js)
+[Verkehrsverbund Bremen/Niedersachsen (VBN)](https://de.wikipedia.org/wiki/Verkehrsverbund_Bremen/Niedersachsen) | - | [docs](p/vbn/readme.md) | [example code](p/vbn/example.js) | [src](p/vbn/index.js)
 
 There are also libraries that use `hafas-client` and pass their own profile in:
 
