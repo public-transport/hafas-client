b9d5c85a5435d6cbb944fc679ffea7bf4b219286
Jannis R <mail@jannisr.de>
2020-03-07 00:26:42 +0100

    tests: DB stop(), INSA stop() :white_check_mark:

---
+++ test/fixtures/db-stop.js
@@ -0,0 +1,25 @@
+'use strict'
+
+module.exports = {
+	type: 'stop',
+	id: '8011155',
+	name: 'Berlin Alexanderplatz',
+	location: {
+		type: 'location',
+		id: '8011155',
+		latitude: 52.521526,
+		longitude: 13.411088
+	},
+	products: {
+		nationalExpress: false,
+		national: false,
+		regionalExp: false,
+		regional: true,
+		suburban: true,
+		bus: true,
+		ferry: false,
+		subway: true,
+		tram: true,
+		taxi: false
+	}
+}
---
+++ test/fixtures/insa-stop.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = {
+	type: 'stop',
+	id: '7341',
+	name: 'Magdeburg, S-Bahnhof SKET Industriepark',
+	location: {
+		type: 'location',
+		id: '7341',
+		latitude: 52.096849,
+		longitude: 11.637444
+	},
+	products: {
+		nationalExpress: false,
+		national: false,
+		regional: false,
+		suburban: false,
+		tram: false,
+		bus: true,
+		tourismTrain: false
+	},
+}
