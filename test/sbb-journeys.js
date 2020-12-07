5b1010562a7b77201f5788a1997fe6c999199c02
Jannis R <mail@jannisr.de>
2020-12-07 14:16:57 +0100

    parseJourneyLeg parseAlternative: handle missing stopL[] 🐛, add tests ✅

---
+++ test/sbb-journeys.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const test = require('tape')
+
+const createClient = require('..')
+const rawProfile = require('../p/sbb')
+const res = require('./fixtures/sbb-journeys.json')
+const expected = require('./fixtures/sbb-journeys.js')
+
+const client = createClient(rawProfile, 'public-transport/hafas-client:test')
+const {profile} = client
+
+const opt = {
+	results: null,
+	via: null,
+	stopovers: false,
+	transfers: -1,
+	transferTime: 0,
+	accessibility: 'none',
+	bike: false,
+	walkingSpeed: 'normal',
+	startWithWalking: true,
+	tickets: false,
+	polylines: false,
+	subStops: true,
+	entrances: true,
+	remarks: true,
+	scheduledDays: false,
+	departure: '2020-12-07T13:29+01:00',
+	products: {},
+}
+
+test('parses a journeys() response correctly (SBB)', (t) => {
+	const common = profile.parseCommon({profile, opt, res})
+	const ctx = {profile, opt, common, res}
+	const journeys = res.outConL.map(j => profile.parseJourney(ctx, j))
+
+	t.deepEqual(journeys, expected)
+	t.end()
+})
