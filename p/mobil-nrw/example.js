de6825e55fc470f05f3c882e4691eb693f49acf0
Jannis R <mail@jannisr.de>
2021-01-10 17:46:09 +0100

    add mobil.nrw profile

---
+++ p/mobil-nrw/example.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const {inspect} = require('util')
+const createClient = require('../..')
+const cflProfile = require('.')
+
+const client = createClient(cflProfile, 'hafas-client-example')
+
+const soest = '8000076'
+const aachenHbf = '8000001'
+
+client.journeys(soest, aachenHbf, {results: 1, stopovers: true})
+// .then(({journeys}) => {
+// 	const leg = journeys[0].legs[0]
+// 	return client.trip(leg.tripId, leg.line.name)
+// })
+
+// client.locations('soest', {results: 3})
+// client.reachableFrom({
+// 	type: 'location',
+// 	id: '980301639',
+// 	latitude: 51.387609,
+// 	longitude: 6.684019,
+// 	address: 'Duisburg, Am Mühlenberg 1',
+// }, {
+// 	maxDuration: 15,
+// })
+// client.nearby({
+// 	type: 'location',
+// 	latitude: 51.4503,
+// 	longitude: 6.6581,
+// }, {distance: 1200})
+
+// client.station(soest)
+// client.departures(soest, {duration: 20})
+
+// client.radar({
+// 	north: 51.4358,
+// 	west: 6.7625,
+// 	south: 51.4214,
+// 	east: 6.7900,
+// }, {results: 10})
+
+// client.remarks()
+
+.then(data => {
+	console.log(inspect(data, {depth: null, colors: true}))
+})
+.catch(console.error)
---
+++ p/mobil-nrw/products.js
@@ -0,0 +1,78 @@
+'use strict'
+
+module.exports = [
+	{
+		id: 'regional-train',
+		mode: 'train',
+		bitmasks: [8],
+		// todo: specify explicitly which
+		name: 'regional train',
+		short: 'regional train',
+		default: true,
+	},
+	{
+		id: 'urban-train',
+		mode: 'train',
+		bitmasks: [16],
+		name: 'urban train',
+		short: 'urban train',
+		default: true,
+	},
+	{
+		id: 'subway',
+		mode: 'train',
+		bitmasks: [128],
+		name: 'subway',
+		short: 'subway',
+		default: true,
+	},
+	{
+		id: 'tram',
+		mode: 'train',
+		bitmasks: [256],
+		name: 'tram',
+		short: 'tram',
+		default: true,
+	},
+	{
+		id: 'bus',
+		mode: 'bus',
+		bitmasks: [32],
+		name: 'bus',
+		short: 'bus',
+		default: true,
+	},
+	{
+		id: 'dial-a-ride',
+		mode: 'taxi',
+		bitmasks: [512],
+		name: 'dial-a-ride',
+		short: 'dial-a-ride',
+		default: true,
+	},
+	{
+		id: 'long-distance-train',
+		mode: 'train',
+		bitmasks: [4],
+		// todo: specify explicitly which
+		name: 'long-distance train',
+		short: 'long-distance train',
+		default: true,
+	},
+	{
+		id: 'express-train',
+		mode: 'train',
+		bitmasks: [1],
+		name: 'ICE',
+		short: 'ICE',
+		default: true,
+	},
+	{
+		id: 'ec-ic',
+		mode: 'ec-ic',
+		bitmasks: [2],
+		name: 'EC/IC',
+		short: 'EC/IC',
+		default: true,
+	}
+]
