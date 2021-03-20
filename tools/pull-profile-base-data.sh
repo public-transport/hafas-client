1f8e3c8d73d465315c6af606f7cf1125abcbd5c7
Jannis R <mail@jannisr.de>
2021-03-20 19:39:49 +0100

    INVG: use JSON base profile

--- tools/pull-profile-base-data.sh
+++ tools/pull-profile-base-data.sh
@@ -26,3 +26,4 @@ jq -r --tab "$query" <"$src/de/db-hafas-mgate.json" >../p/db/base.json
 jq -r --tab "$query" <"$src/de/db-busradar-nrw-hafas-mgate.json" >../p/db-busradar-nrw/base.json
 jq -r --tab "$query" <"$src/de/hvv-hafas-mgate.json" >../p/hvv/base.json
 jq -r --tab "$query" <"$src/de/nasa-hafas-mgate.json" >../p/insa/base.json
+jq -r --tab "$query" <"$src/de/invg-hafas-mgate.json" >../p/invg/base.json
