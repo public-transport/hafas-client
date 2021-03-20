8ac23e861f4bc66fb7ad112adb466ad7c15a12c4
Jannis R <mail@jannisr.de>
2021-03-20 18:39:21 +0100

    CFL: use JSON base profile

--- p/cfl/index.js
+++ p/cfl/index.js
@@ -1,26 +1,19 @@
 'use strict'
 
+const baseProfile = require('./base.json')
 const products = require('./products')
 
 const transformReqBody = (ctx, body) => {
-	body.client = {
-		type: 'IPH',
-		id: 'HAFAS',
-		v: '4000000',
-		name: 'cflPROD-STORE',
-		os: 'iPhone OS 9.3.5'
-	}
-	body.ver = '1.16'
-	body.auth = {aid: 'ALT2vl7LAFDFu2dz'}
 	body.lang = 'de'
 
 	return body
 }
 
 const cflProfile = {
+	...baseProfile,
+
 	locale: 'de-LU',
 	timezone: 'Europe/Luxembourg',
-	endpoint: 'https://horaires.cfl.lu/bin/mgate.exe',
 	transformReqBody,
 
 	products: products,
