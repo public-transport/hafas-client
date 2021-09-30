cec22170c53ab3dc06cb422fdb5c0c0d548c3e66
Jannis R <mail@jannisr.de>
2021-09-30 22:16:24 +0200

    rest.exe parseLocation: parse products, add type: station

--- parse-rest/location.js
+++ parse-rest/location.js
@@ -24,12 +24,16 @@ const parseLocation = (ctx, l) => {
 	// todo: l.notes https://github.com/public-transport/hafas-client/issues/130
 
 	if (l.type === 'S' || l.type === 'ST' || id.A === '1') {
+		// todo: l.altId, l.mainMastAltId
+
 		const stop = {
 			type: 'stop',
 			id: res.id,
 			name: l.name || id.O ? profile.parseStationName(ctx, l.name || id.O) : null,
 			location: 'number' === typeof res.latitude ? res : null
 		}
+
+		if ('products' in l) stop.products = profile.parseProductsBitmask(ctx, l.products)
 
 		if (opt.linesOfStops && Array.isArray(l.productAtStop)) {
 			stop.lines = l.productAtStop.map(p => profile.parseLine(ctx, {
@@ -44,6 +48,8 @@ const parseLocation = (ctx, l) => {
 				extId: l.mainMastExtId
 			})
 			stop.station.type = 'station'
+		} else {
+			stop.type = 'station'
 		}
 
 		return stop
