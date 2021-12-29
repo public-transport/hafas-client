aed82c21dbe1dcd4cb3badb8d85167a58934821a
Jannis R <mail@jannisr.de>
2021-12-29 21:26:12 +0100

    lines: realtimeDataFrom -> realtimeDataUpdatedAt 💥✅📝

---
+++ test/e2e/lib/lines.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const testLines = async (cfg) => {
+	const {
+		test: t,
+		fetchLines,
+		validate,
+		query,
+	} = cfg
+
+	const res = await fetchLines(query)
+	const {
+		lines,
+		realtimeDataUpdatedAt,
+	} = res
+
+	for (let i = 0; i < res.lines.length; i++) {
+		const l = res.lines[i]
+		const name = `res.lines[${i}]`
+		validate(t, l, 'line', name)
+	}
+
+	validate(t, realtimeDataUpdatedAt, 'realtimeDataUpdatedAt', 'res.realtimeDataUpdatedAt')
+}
+
+module.exports = testLines
