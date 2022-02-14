f5f4091ba54ca6c0126d665f5f24cc4b12ec4f8e
Jannis R <mail@jannisr.de>
2022-02-14 15:22:35 +0100

    VBB rest.exe: parse DHID/IFOPT stop IDs [todo:test]

--- p/vbb-rest/index.js
+++ p/vbb-rest/index.js
@@ -1,11 +1,25 @@
 import pick from 'lodash/pick.js'
 import {profile as vbbMgateProfile} from '../vbb/index.js'
+import {parseHook} from '../../lib/profile-hooks.js'
+import {parseLocation as _parseLocation} from '../../parse-rest/location.js'
 
 const TEST_ENDPOINT = 'https://vbb.demo.hafas.de/fahrinfo/restproxy/2.15/'
 const PRODUCTION_ENDPOINT = 'https://fahrinfo.vbb.de/restproxy/2.15/'
 const ENDPOINT = process.env.NODE_ENV === 'production'
 	? PRODUCTION_ENDPOINT
 	: TEST_ENDPOINT
+
+const dhidRegex = /^A×[a-z]{2,3}:./u
+const dhidPrefix = 'A×'
+const parseLocationWithStopDHID = ({parsed: stop}, l) => {
+	const dhid = Array.isArray(l.altId) && l.altId.find(id => dhidRegex.test(id))
+	if (dhid) {
+		if (!stop.ids) stop.ids = {}
+		stop.ids.dhid = dhid.slice(dhidPrefix.length)
+	}
+
+	return stop
+}
 
 const vbbRestProfile = {
 	...pick(vbbMgateProfile, [
@@ -15,6 +29,8 @@ const vbbRestProfile = {
 	]),
 
 	endpoint: ENDPOINT,
+
+	parseLocation: parseHook(_parseLocation, parseLocationWithStopDHID),
 }
 
 export {
