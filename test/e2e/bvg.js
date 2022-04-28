57ff2cef271b7e45aa11a93aaad04be4374811d8
Jannis R <mail@jannisr.de>
2022-04-28 11:47:23 +0200

    BVG: don't convert IDs 💥✅

--- test/e2e/bvg.js
+++ test/e2e/bvg.js
@@ -43,13 +43,13 @@ const validate = createValidate(cfg, {
 
 const client = createClient(bvgProfile, 'public-transport/hafas-client:test')
 
-const amrumerStr = '900000009101'
-const spichernstr = '900000042101'
-const bismarckstr = '900000024201'
-const westhafen = '900000001201'
-const wedding = '900000009104'
-const württembergallee = '900000026153'
-const tiergarten = '900000003103'
+const amrumerStr = '900009101'
+const spichernstr = '900042101'
+const bismarckstr = '900024201'
+const westhafen = '900001201'
+const wedding = '900009104'
+const württembergallee = '900026153'
+const tiergarten = '900003103'
 const jannowitzbrücke = '900000100004'
 
 const hour = 60 * 60 * 1000
@@ -163,7 +163,7 @@ tap.skip('journeys: walkingSpeed', async (t) => {
 		latitude: 52.443576,
 		longitude: 13.198973
 	}
-	const wannsee = '900000053301'
+	const wannsee = '900053301'
 
 	await testJourneysWalkingSpeed({
 		test: t,
@@ -357,8 +357,8 @@ tap.test('arrivals', async (t) => {
 })
 
 tap.test('nearby', async (t) => {
-	const berlinerStr = '900000044201'
-	const landhausstr = '900000043252'
+	const berlinerStr = '900044201'
+	const landhausstr = '900043252'
 
 	// Berliner Str./Bundesallee
 	const nearby = await client.nearby({
