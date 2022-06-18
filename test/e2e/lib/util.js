ede4341a2384529c3e52c907ffb0d5852cb8d54c
Jannis R <mail@jannisr.de>
2022-06-18 16:50:33 +0200

    integration test mocking: replayer -> Polly.js ✅

--- test/e2e/lib/util.js
+++ test/e2e/lib/util.js
@@ -38,17 +38,55 @@ const assertValidWhen = (actual, expected, name, delta = day + 6 * hour) => {
 // HTTP request mocking
 if (process.env.VCR_MODE && !process.env.VCR_OFF) {
 	const require = createRequire(import.meta.url)
-	const replayer = require('replayer')
 
-	replayer.configure({
-		headerWhitelist: [
-			// excludes User-Agent & Connection
-			'Content-Type', 'Accept-Encoding', 'Accept',
-		],
-		includeHeaderValues: true,
-		touchHits: false,
+	const {Polly} = require('@pollyjs/core')
+	const NodeHttpAdapter = require('@pollyjs/adapter-node-http')
+	const FSPersister = require('@pollyjs/persister-fs')
+	const tap = require('tap')
+
+	Polly.register(NodeHttpAdapter)
+	Polly.register(FSPersister)
+
+	let mode
+	if (process.env.VCR_MODE === 'record') mode = 'record'
+	else if (process.env.VCR_MODE === 'playback') mode = 'replay'
+	else throw new Error('invalid $VCR_MODE, must be "record" or "replay"')
+
+	const polly = new Polly('requests', {
+		logLevel: 'warn',
+		// If a request's recording is not found, pass-through to the server and record the response.
+		recordIfMissing: false,
+		// If false, Polly will throw when attempting to persist any failed requests. A request is considered to be a failed request when its response's status code is ≥ 400.
+		recordFailedRequests: true,
+		// Await any unresolved requests handled by the polly instance (via flush) when stop is called.
+		flushRequestsOnStop: true,
+		// The Polly mode. Can be one of the following:
+		// - replay: Replay responses from recordings.
+		// - record: Force Polly to record all requests. This will overwrite recordings that already exist.
+		// - passthrough: Passes all requests through directly to the server without recording or replaying.
+		mode,
+		adapters: ['node-http'],
+		persister: 'fs',
+		persisterOptions: {
+			fs: {
+				recordingsDir: new URL('../fixtures', import.meta.url).pathname,
+			},
+			// When disabled, requests that have not been captured by the running Polly instance will be removed from any previous recording. This ensures that a recording will only contain the requests that were made during the lifespan of the Polly instance. When enabled, new requests will be appended to the recording file.
+			keepUnusedRequests: true, // todo: change to false?
+		},
+		matchRequestsBy: {
+			headers: (headers, req) => {
+				// todo: use an allow-list here?
+				delete headers['user-agent'] // randomised
+				delete headers['connection'] // not relevant for tests
+				return headers
+			},
+		},
 	})
-	replayer.fixtureDir(new URL('../fixtures', import.meta.url).pathname)
+
+	tap.teardown(async () => {
+		await polly.stop()
+	})
 }
 
 export {
