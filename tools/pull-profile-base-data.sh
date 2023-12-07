0102b720090970ef9bc7ccc1271b325ab8b294f5
Jannis R <mail@jannisr.de>
2023-12-07 01:05:43 +0100

    remove defunct SNCB profile 💥📝✅
    related: #284
    related: https://github.com/iRail/iRail/issues/466
    related: https://github.com/schildbach/public-transport-enabler/issues/502

--- tools/pull-profile-base-data.sh
+++ tools/pull-profile-base-data.sh
@@ -49,7 +49,6 @@ node -p "$query" "$src/de/rmv-hafas-mgate.json" >../p/rmv/base.json
 node -p "$query" "$src/de/rsag-hafas-mgate.json" >../p/rsag/base.json
 node -p "$query" "$src/de/saarvv-hafas-mgate.json" >../p/saarfahrplan/base.json
 node -p "$query" "$src/de/db-sbahn-muenchen-hafas-mgate.json" >../p/sbahn-muenchen/base.json
-node -p "$query" "$src/be/nmbs-sncb-hafas-mgate.json" >../p/sncb/base.json
 node -p "$query" "$src/at/svv-hafas-mgate.json" >../p/svv/base.json
 node -p "$query" "$src/de/vbb-hafas-mgate.json" >../p/vbb/base.json
 node -p "$query" "$src/de/vbn-hafas-mgate.json" >../p/vbn/base.json
