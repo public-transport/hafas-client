4751df344b7fadb370ca5ff539b0f803e25c57f7
Jannis R <mail@jannisr.de>
2025-02-14 00:20:32 +0100

    PKP: fix by using a specific User-Agent 🐛

--- p/pkp/index.js
+++ p/pkp/index.js
@@ -9,6 +9,9 @@ import {parseLocation} from '../../parse/location.js';
 const baseProfile = require('./base.json');
 import {products} from './products.js';
 
+// https://github.com/public-transport/hafas-client/issues/184#issuecomment-2646119337
+const PKP_USER_AGENT = 'Dalvik/2.1.0';
+
 const trimStopName = ({parsed}, l) => {
 	if (parsed.type === 'stop' || parsed.type === 'station' && parsed.name) {
 		parsed.name = parsed.name.replace(/(^-|-$)/g, '');
@@ -16,10 +19,17 @@ const trimStopName = ({parsed}, l) => {
 	return parsed;
 };
 
+const transformReqOverrideUserAgent = (ctx, req) => {
+	req.headers['user-agent'] = PKP_USER_AGENT
+	return req
+}
+
 const profile = {
 	...baseProfile,
 	locale: 'pl-PL',
 	timezone: 'Europe/Warsaw',
+	randomizeUserAgent: false,
+	transformReq: transformReqOverrideUserAgent,
 
 	products,
 
