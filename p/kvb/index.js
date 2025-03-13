e6bdbfb25fb42e40d63c4ebe25c0418a61194bb1
Jannis R <mail@jannisr.de>
2025-03-13 15:58:43 +0100

    KVB: provide hard-coded SSL CA certificate 🐛

--- p/kvb/index.js
+++ p/kvb/index.js
@@ -3,6 +3,8 @@
 import {createRequire} from 'module';
 const require = createRequire(import.meta.url);
 
+import {readFileSync} from 'fs';
+import {Agent} from 'https';
 const baseProfile = require('./base.json');
 
 const products = [{
@@ -49,8 +51,16 @@ const products = [{
 	default: true,
 }];
 
+// `auskunft.kvb.koeln/gate` doesn't provide the necessary CA certificate chain for Node.js to trust the certificate, so we manually augment it.
+// I have let them know on 2025-03-13, let's see when they fix it.
+const ca = readFileSync(new URL('./thawte-tls-rsa-ca-g1.crt.pem', import.meta.url).pathname);
+const agent = new Agent({ca});
+const transformReq = (ctx, req) => ({...req, agent});
+
 const profile = {
 	...baseProfile,
+	transformReq,
+
 	locale: 'de-DE',
 	timezone: 'Europe/Berlin',
 
